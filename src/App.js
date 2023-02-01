2a504065fcd6b49f785f5a2d6e772235b25fa13c
Archilg456 <gabaidze555@gmail.com>
2023-02-02 00:38:16 +0400

    --

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  let users = [
+    { name: 'User 1', lname: userLname,  age: 25 },
+    { name: 'User 2', lname: userLname,  age: 30 },
+    { name: 'User 3', lname: userLname,  age: 35 }
+  ];
+
+
+  return (
+    <div className='Parent '>
+
+    <div className='Child'>
+
+
+    </div>
+
+    </div>
+
+  );
+}
+
+export default App;
